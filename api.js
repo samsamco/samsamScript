8d6ea278506d89331f892d77a8794beb4b4941dd
samsamco <contact@samsam.ma>
2019-04-26 10:05:05 +0100

    push update

--- api.js
+++ api.js
@@ -1,4 +1,10 @@
+/*!
+  * Bootstrap v4.0.0 (https://getbootstrap.com)
+  * Copyright 2011-2018 The Bootstrap Authors (https://github.com/twbs/bootstrap/graphs/contributors)
+  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
+  */
 
+  
 //---------twilio sms-------
 
 function sendSms_Api(code,gsm) {
