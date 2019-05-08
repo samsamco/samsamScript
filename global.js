f8df4c98a5e0d68e20b0952f8c16ca875275bc6c
samsamco <contact@samsam.ma>
2019-05-08 10:40:09 +0000

    Create global.js

---
+++ global.js
@@ -0,0 +1,6 @@
+landing_page_source = '{{ landing_page_source }}';
+ID_google_analytic = '{{ ID_google_analytic }}';
+ID_google_Ad = '{{ ID_google_Ad }}';
+ID_google_View = '{{ ID_google_View }}';
+Userip = "{{ app.request.server.get("REMOTE_ADDR") }}"
+Token = new Date().getTime();
